5295228b7f58b0dafd274dfe5f4c931f4f3e63dc
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-06-30 12:56:35 -0400

    Add files via upload

---
+++ 1_dev__stage/V1.1.5__ADDRESSES_POPULATE.sql
@@ -0,0 +1,9 @@
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('136 N ParkRidge Rd', 'Bloomington', 'Indiana');
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('138 N ParkRidge Rd', 'Bloomington', 'Indiana');
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('140 N ParkRidge Rd', 'Bloomington', 'Indiana');
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('142 N ParkRidge Rd', 'Bloomington', 'Indiana');
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('144 N ParkRidge Rd', 'Bloomington', 'Indiana');
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('146 N ParkRidge Rd', 'Bloomington', 'Indiana');
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('148 N ParkRidge Rd', 'Bloomington', 'Indiana');
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('150 N ParkRidge Rd', 'Bloomington', 'Indiana');
+insert into DEMO.ADDRESSES (STREET_ADDRESS, CITY, STATE) values ('152 N ParkRidge Rd', 'Bloomington', 'Indiana');
